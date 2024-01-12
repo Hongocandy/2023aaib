95838ace2c0755028b0d8b18faa79137fc7b90d7
Hongocandy <12750076@me.mcu.edu.tw>
2024-01-12 11:33:53 +0800

    week18

---
+++ week18/week18-3.py
@@ -0,0 +1,8 @@
+=10
+b=True
+c='a'
+d=3.14
+print(a,b,c,d)#print()輸出
+print(a,b,c,d,sep=' ::: ',end='')
+e=input('請輸入一個字串')#input()輸入
+print(e)
---
+++ week18/week18-4.py
@@ -0,0 +1,7 @@
+#資料類別
+a=10
+print( type(a),a)
+a='123'
+print(type(a),a)
+a=3.14
+print(type(a),a)
